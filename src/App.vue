b8e237d4b612e33d2c6fe6342275bf341fb03886
Théo <theo.dancoisne@free.fr>
2022-05-17 13:42:22 +0200

    basics

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="bottom-screen">
+        <div class="led-screen">
+            <div class="led-color">
+                <div id="bottom-screen-body">
+                    azertyui
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>{{pokemonsData}}</div>
+</template>
+
+<script>
+//import { reactive } from "vue";
+import axios from "axios";
+import style from "./stylesheet.css";
+
+export default {
+    data() {
+        return {
+            pokemonsData: null
+        }
+    },
+    async mounted() {
+        await axios.get("https://pokeapi.co/api/v2/pokemon/1").then(res => (this.pokemonsData = res));
+    }
+}
+</script>
