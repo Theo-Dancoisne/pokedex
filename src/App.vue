80633fbe92b81712166455e5ac6871311718c92e
Théo <theo.dancoisne@free.fr>
2022-05-25 11:24:21 +0200

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div id="pokedex">
-        <div id="top-screen-support">
-            <div class="top-grey-support"><div class="hole top-screen-hole"></div></div>
-            <div class="screen">
-                <div class="led-background-image">
-                    <div class="led-background-color">
-                        <div id="top-screen-body">
-                            <TopScreen :pokemonURL="pokemonURL"/>
+        <div id="top-screen-container">
+            <div id="speaker">
+                <div></div>
+                <div id="speaker-cache"></div>
+                <div>
+                    <div id="speaker-top-hole" class="hole"></div>
+                    <div id="speaker-right-hole" class="hole"></div>
+                    <div id="speaker-bottom-hole" class="hole"></div>
+                    <div id="speaker-left-hole" class="hole"></div>
+                </div>
+            </div>
+            <div id="top-screen-support">
+                <div class="top-grey-support"><div class="hole top-screen-hole"></div></div>
+                <div class="screen">
+                    <div class="led-background-image">
+                        <div class="led-background-color">
+                            <div id="top-screen-body">
+                                <TopScreen :pokemonURL="pokemonURL"/>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -15,13 +27,23 @@
         <div id="separator">
             <span><span><span></span></span></span>
         </div>
-        <div id="bottom-screen-support">
-            <div class="bottom-grey-support"><div class="little-button"></div><div class="little-button"></div></div>
-            <div class="screen">
-                <div class="led-background-image">
-                    <div class="led-background-color">
-                        <div id="bottom-screen-body">
-                            <MainScreen @display-pokemon="displayPokemon"/>
+        <div id="bottom-screen-container">
+            <div id="directional-keys">
+                <div><div id="keys">
+                    <object id="dirkeyTop" :data="directional_keys+'#top'"></object>
+                    <object id="dirkeyRight" :data="directional_keys+'#right'"></object>
+                    <object id="dirkeyBottom" :data="directional_keys+'#bottom'"></object>
+                    <object id="dirkeyLeft" :data="directional_keys+'#left'"></object>
+                </div></div>
+            </div>
+            <div id="bottom-screen-support">
+                <div class="bottom-grey-support"><div class="little-button"></div><div class="little-button"></div></div>
+                <div class="screen">
+                    <div class="led-background-image">
+                        <div class="led-background-color">
+                            <div id="bottom-screen-body">
+                                <MainScreen @display-pokemon="displayPokemon"/>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -45,6 +67,7 @@ export default {
     data() {
         return {
             pokemonURL: null,
+            directional_keys: "./src/ressources/directional_keys.svg",
         }
     },
     methods: {
--- src/vues/top-screen.vue
+++ src/vues/top-screen.vue
@@ -1,6 +1,17 @@
 <template>
-    <div class="porkemonSprite">
-        <img :src="sprite" alt="pokemon sprite">
+    <div id="porkemonData">
+        <div>
+            <h3>Types :</h3>
+            <!-- <span>{{pokemonData.value.types[0].type.name}}</span> -->
+            <span>{{ pokemonSumUp.types }}</span>
+            <h3>Abilities :</h3>
+            <span>{{ pokemonSumUp.abilities }}</span>
+            <h3>Height :</h3>
+            <span class="number">{{ pokemonSumUp.height }} dec</span>
+            <h3>Weight :</h3>
+            <span class="number">{{ pokemonSumUp.weight }} hec</span>
+        </div>
+        <img :src="pokemonSumUp.sprite" alt="pokemon sprite">
     </div>
 </template>
 
@@ -10,14 +21,21 @@ import axios from "axios";
 
 const props = defineProps(["pokemonURL"]);
 const pokemonData = reactive({});
-const sprite = ref(null);
+const pokemonSumUp = reactive({types: "", height: null, weight: null, sprite: null, abilities: ""});
 watch(props, () => {
     if (props.pokemonURL) start();
 });
 
 async function start() {
+    // ‼ don't reset variables before this following sync function ‼
     await axios.get(props.pokemonURL).then(res => (pokemonData.value = res));
     pokemonData.value = await pokemonData.value.data;
-    sprite.value = pokemonData.value.sprites.front_default;
+    pokemonSumUp.types = pokemonSumUp.abilities = "";
+    pokemonSumUp.sprite = pokemonData.value.sprites.front_default;
+    // sprite.value = pokemonData.value.sprites.versions["generation-i"]["red-blue"].front_transparent;
+    for (var i=0; i < pokemonData.value.types.length; i++) {pokemonSumUp.types += pokemonData.value.types[i].type.name + " ";}
+    for (var i=0; i < pokemonData.value.abilities.length; i++) {pokemonSumUp.abilities += pokemonData.value.abilities[i].ability.name + " ";}
+    pokemonSumUp.height = pokemonData.value.height;
+    pokemonSumUp.weight = pokemonData.value.weight;
 }
 </script>
